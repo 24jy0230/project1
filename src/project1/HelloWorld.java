0d27bd7c3e4c3b20da1f942adf021c6790364658
24jy0230 <24jy0230@PC03.jynet2.jec.ac.jp>
2025-09-25 12:25:54 +0900

    日本電子専門学校を10回表示する

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -3,7 +3,9 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("日本電子専門学校");
+		for(int i=0; i<10; i++) {
+			System.out.println("日本電子専門学校");
+		}
 	}
 
 }
