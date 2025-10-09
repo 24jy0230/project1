695ee090f4ed52a2ffe1694edf525eeeccb9768b
24jy0230 Fujimura Kazuki <24jy0230@jec.ac.jp>
2025-10-09 12:19:01 +0900

    HelloWorldを３行表示

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -3,9 +3,9 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++) {
-			System.out.println("日本電子専門学校");
-		}
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
 	}
 
 }
